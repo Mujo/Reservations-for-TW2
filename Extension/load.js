c74796b6b384ea8218dba6d4f6648231f4b7ab8a
Ney Fonseca <mujogt@gmail.com>
2016-03-21 19:58:57 -0300

    finally cleaning

--- Extension/load.js
+++ Extension/load.js
@@ -4,32 +4,6 @@ function injectJs(link) {
         scr.src=link;
         (document.head || document.body || document.documentElement).appendChild(scr);
 }
-//console.log('chrome.storage', chrome.storage);
-//console.log('chrome.runtime.onMessage', chrome.runtime.onMessage);
+
 injectJs(chrome.extension.getURL("utils.js"));
 injectJs(chrome.extension.getURL("reservations.js"));
-var private = {};
-
-//window.chrome = chrome;
-// chrome.runtime.onMessage.addListener(function (msg, sender, callback) {
-// 	//console.log("chegou no load", msg, sender, callback);
-// 	msg.status = "load";
-// 	msg.sender = sender;
-// 	callback(msg);
-// 	return true;
-// });
-
-// window.onMessageHandler = function (msg, sender, callback) {
-// 	console.log("chegou no load", msg);
-// 	//msg.status = "load";
-// 	//msg.sender = sender;
-// 	callback(msg);
-// };
-// chrome.runtime.onMessage.addListener(window.onMessageHandler);
-// 
-// chrome.runtime.sendMessage(
-//     "foo",
-//     function (response) {
-//         console.log(response);
-//     }
-// );
