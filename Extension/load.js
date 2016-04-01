1e86128123239e0f8b316cb2907c14047cf9b7de
Ney Fonseca <mujogt@gmail.com>
2016-03-31 22:34:51 -0300

    v0.1.9 - a lot of changes... :P

--- Extension/load.js
+++ Extension/load.js
@@ -1,9 +1,14 @@
 function injectJs(link) {
-        var scr = document.createElement("script");
-        scr.type="text/javascript";
-        scr.src=link;
-        (document.head || document.body || document.documentElement).appendChild(scr);
+	try {
+		var scr = document.createElement("script");
+		scr.type = "text/javascript";
+		scr.src = link;
+		(document.head || document.body || document.documentElement).appendChild(scr);
+	} catch (error) {
+		setTimeout(injectJs, 200, link);
+	}
 }
+
 
 injectJs(chrome.extension.getURL("utils.js"));
 injectJs(chrome.extension.getURL("reservations.js"));
--- Extension/reservations.js
+++ Extension/reservations.js
@@ -1,24 +1,25 @@
 ﻿// Reservations for TW2
 var reservations = window.reservations = (function() {
 	var debug = true,
-		reservGroupIcons = [1802, 2570],
-		grpReserv,
+		reservGroupIcons = [2068, 2067],
+		grpReserved,
 		grpMyReserv,
 		charId,
 		charName,
 		worldId,
-		dbversion = 1;
+		dbversion = 2;
 
 	var log = function() {
 		if (debug)
 			console.log.apply(console, arguments);
 	};
 
-	// Database
+	// Database Model
 	var db = function() {
 		return {
 			messages: {},
-			reservations: {}
+			reservations: {},
+			groups: {}
 		};
 	} ();
 
@@ -27,10 +28,7 @@ var reservations = window.reservations = (function() {
 		var player;
 		checkIfLoaded(function() {
 			player = window.modelDataService.getPlayer();
-			if (player && player.data)
-				return true;
-			else
-				return false;
+			return (player && player.data);
 		}, function() {
 			var charData = player.data.selectedCharacter.data;
 			charId = charData.character_id;
@@ -38,14 +36,32 @@ var reservations = window.reservations = (function() {
 			worldId = charData.world_id;
 			var localDbMsgs = localStorage["reservDB_v" + dbversion + "_" + worldId + "_" + charId];
 			db = (localDbMsgs) ? JSON.parse(localDbMsgs) : db;
-			log("reservDB loaded.");
+			log("reservDB loaded.", db);
 		});
 	}
 
 	var saveDB = function() {
 		log("saving reservDB...");
 		localStorage["reservDB_v" + dbversion + "_" + worldId + "_" + charId] = JSON.stringify(db);
-		log("reservDB saved.");
+		log("reservDB saved.", db);
+	}
+
+	var cleanDB = function() {
+		for (var r in db.reservations) {
+			var msg_id = db.reservations[r].msg_id;
+			if (!db.messages[msg_id]) {
+				log("cleaning db message: ", msg_id, " reservation:", db.reservations[r]);
+				delete db.reservations[r];
+			}
+		}
+		saveDB();
+	}
+
+	var eraseOldLocalDB = function() {
+		Object.keys(localStorage).filter(f => f.indexOf("reservDB_") > -1 && f.indexOf("reservDB_v" + dbversion + "_") < 0).forEach(e => {
+			log("removing old db:", e, JSON.parse(localStorage[e]));
+			localStorage.removeItem(e);
+		});
 	}
 
 	// SET Services and Handlers
@@ -102,16 +118,16 @@ var reservations = window.reservations = (function() {
 		}
 	}
 
+	// Messages
 	var onOpenMessage = function(id) {
 		checkIfLoaded('[ng-controller="MessageViewController"]', function() {
 			window.MessageViewController = window.angular.element($('[ng-controller="MessageViewController"]')).scope();
-			window.MessageViewController.$on(window.eventTypeProvider.MESSAGE_VIEW, onMessageLoaded);
+			window.MessageViewController.$on(window.eventTypeProvider.MESSAGE_VIEW, onMessageView);
 		});
 	}
 
-	// Messages
-	var onMessageLoaded = function(event, data) {
-		log("onMessageView", event, data);
+	var onMessageView = function(_, data) {
+		log("onMessageView", data);
 		checkIfLoaded('[ng-controller="MessageViewController"] ul.list-center', function() {
 			var msg_owner = (data.author_id === charId);
 			var msg = db.messages[data.message_id];
@@ -139,11 +155,11 @@ var reservations = window.reservations = (function() {
 					msg.active = reservActive.attr("class")[0].split(" ").find(i => i == "icon-26x26-checkbox-checked") ? 1 : 0;
 					var send_msg;
 					if (msg.active) {
-						send_msg = "/startreserv ";
-						window.messagingService.reply(msg.id, send_msg + hash(send_msg + JSON.stringify(msg)));
+						send_msg = "/startreserv " + zip(data.author_id);
+						window.messagingService.reply(msg.id, send_msg);
 					} else {
 						if (confirm("Do you like to exclude all reserves from this message? This action can not be undone!")) {
-							send_msg = "/cleanreserv ";
+							send_msg = "/cleanreserv";
 							delete db.messages[data.message_id];
 							var timeout = (2000 + Math.round(Math.random() * 4000));
 							for (var vid in db.reservations) {
@@ -156,15 +172,15 @@ var reservations = window.reservations = (function() {
 										} else {
 											dettachVillage(reservId, villageId);
 										}
-									}, timeout, isMine, window.gameDettachGroupFromVillage, grpMyReserv.id, grpReserv.id, vid);
+									}, timeout, isMine, window.gameDettachGroupFromVillage, grpMyReserv.id, grpReserved.id, vid);
 									delete db.reservations[vid];
 									timeout += (2000 + Math.round(Math.random() * 4000));
 								}
 							}
 						} else {
-							send_msg = "/stopreserv ";
+							send_msg = "/stopreserv";
 						}
-						window.messagingService.reply(msg.id, send_msg + hash(send_msg + JSON.stringify(msg)));
+						window.messagingService.reply(msg.id, send_msg);
 					}
 
 					saveDB();
@@ -173,8 +189,8 @@ var reservations = window.reservations = (function() {
 		});
 	}
 
-	var onNewMessage = function(event, data) {
-
+	var onNewMessage = function(_, data) {
+		log("onNewMessage", data);
 		var options = {
 			params: data.message.content.trim().split(" "),
 			char_id: data.message.character_id,
@@ -188,15 +204,20 @@ var reservations = window.reservations = (function() {
 	}
 
 	var execCommand = function(options) {
+		var village_id, dt, reserv, msg, reservMsgBtn, reservActive, author_id;
+		if (options.params[0][0] !== "/") return;
 
-		var village_id, dt, sec_hash, reserv, msg, reservMsgBtn, reservActive;
 		var command = options.params[0].slice(1);
+		log("execCommand", command, options);
+
 		switch (command) {
 			case "addreserv":
 				village_id = unzip(options.params[1]);
 				dt = unzip(options.params[2]);
-				sec_hash = options.params[3];
-				if (db.reservations[village_id]) break;
+				if (db.reservations[village_id]) {
+					log("ERROR: reserve for village " + village_id + " already exists!");
+					break;
+				}
 				reserv = {
 					dt: dt,
 					char: {
@@ -205,24 +226,26 @@ var reservations = window.reservations = (function() {
 					},
 					msg_id: options.msg_id
 				};
-				log("addreserv", village_id, dt, sec_hash, reserv);
-				if (sec_hash === hash("/addreserv " + zip(village_id) + " " + zip(dt) + " " + JSON.stringify(reserv))) {
-					db.reservations[village_id] = reserv;
-					saveDB();
-					window.gameAttachGroupToVillage(grpReserv.id, village_id);
-				}
+				log("addreserv", village_id, dt, reserv);
+				db.reservations[village_id] = reserv;
+				saveDB();
+				window.gameAttachGroupToVillage(grpReserved.id, village_id);
 				break;
 
 			case "remreserv":
 				village_id = unzip(options.params[1]);
-				sec_hash = options.params[2];
-				if (!db.reservations[village_id]) break;
+				if (!db.reservations[village_id]) {
+					log("ERROR: reserve for village " + village_id + " not found!");
+					break;
+				}
 				reserv = db.reservations[village_id];
-				log("remreserv", village_id, sec_hash, reserv, options.char_id);
-				if (sec_hash === hash("/remreserv " + zip(village_id) + " " + JSON.stringify(reserv)) && reserv.char.id === options.char_id) {
+				log("remreserv", village_id, reserv, options.char_id);
+				if (reserv.char.id === options.char_id) {
 					delete db.reservations[village_id];
 					saveDB();
-					window.gameDettachGroupToVillage(grpReserv.id, village_id);
+					window.gameDettachGroupToVillage(grpReserved.id, village_id);
+				} else {
+					log("ERROR: this user is not the reserve owner of village: " + village_id + "!");
 				}
 				break;
 
@@ -234,9 +257,9 @@ var reservations = window.reservations = (function() {
 					active: 1,
 					last: options.last
 				};
-				sec_hash = options.params[1];
-				log("startreserv", sec_hash, msg);
-				if (sec_hash === hash("/startreserv " + JSON.stringify(msg))) {
+				author_id = unzip(options.params[1]);
+				log("startreserv", author_id, msg);
+				if (author_id === options.char_id) {
 					db.messages[options.msg_id] = msg;
 					saveDB();
 					if ($(".reserv-msg-btn").length) {
@@ -250,9 +273,8 @@ var reservations = window.reservations = (function() {
 			case "stopreserv":
 				msg = db.messages[options.msg_id];
 				if (!msg) break;
-				sec_hash = options.params[1];
-				log("stopreserv", sec_hash, msg);
-				if (sec_hash === hash("/stopreserv " + JSON.stringify(msg)) && msg.author_id === options.char_id) {
+				log("stopreserv", msg);
+				if (msg.author_id === options.char_id) {
 					msg.active = 0;
 					saveDB();
 					if ($(".reserv-msg-btn").length) {
@@ -266,9 +288,8 @@ var reservations = window.reservations = (function() {
 			case "cleanreserv":
 				msg = db.messages[options.msg_id];
 				if (!msg) break;
-				sec_hash = options.params[1];
-				log("cleanreserv", sec_hash, msg);
-				if (sec_hash === hash("/cleanreserv " + JSON.stringify(msg)) && msg.author_id === options.char_id) {
+				log("cleanreserv", msg);
+				if (msg.author_id === options.char_id) {
 					delete db.messages[options.msg_id];
 					var timeout = (2000 + Math.round(Math.random() * 4000));
 					for (var vid in db.reservations) {
@@ -280,7 +301,7 @@ var reservations = window.reservations = (function() {
 								} else {
 									dettachVillage(reservId, villageId);
 								}
-							}, timeout, isMine, window.gameDettachGroupToVillage, grpMyReserv.id, grpReserv.id, vid);
+							}, timeout, isMine, window.gameDettachGroupToVillage, grpMyReserv.id, grpReserved.id, vid);
 							delete db.reservations[vid];
 							timeout += (2000 + Math.round(Math.random() * 4000));
 						}
@@ -298,6 +319,7 @@ var reservations = window.reservations = (function() {
 
 	var readNewMessages = function() {
 		var newMessages = Object.keys(window.BottomInterfaceController.gameDataModel.data.newMessages);
+		log("readNewMessages", newMessages);
 
 		for (var m in newMessages) {
 			window.messagingService.openMessage(parseInt(m));
@@ -333,8 +355,7 @@ var reservations = window.reservations = (function() {
 
 	// Groups
 	var setGroups = function() {
-		var grps = window.groupService.getGroups();
-		var grpsKeys = Object.keys(grps);
+		var grps, grpsKeys, grpsArr;
 		checkIfLoaded(
 			function() {
 				grps = window.groupService.getGroups();
@@ -342,40 +363,58 @@ var reservations = window.reservations = (function() {
 				return grpsKeys.length > 0;
 			},
 			function() {
-				getGroups(grps);
-				if (!grpMyReserv || !grpReserv) {
+				if (!getGroups(grps)) {
 					log("groups not found.");
 					if (grpsKeys.length > 8) {
 						log("no space for new groups.");
-						if (confirm("Reservations for TW2 needs 2 space for new groups. Do you like to delete the last ones?")) {
-							log("deleting the last ones groups.");
+						if (confirm("Reservations for TW2 needs 2 spaces for new groups. Do you like to delete the last ones?")) {
+							log("deleting groups...");
 							grpsKeys.slice(8).forEach(e => window.groupService.destroyGroup(e));
+						} else {
+							log("groups not created!");
+							return false;
 						}
 					}
-					log("creating groups...");
-					window.groupService.createGroup(reservGroupIcons[0], "My Reserve");
-					window.groupService.createGroup(reservGroupIcons[1], "Reserved");
 
-					grps = window.groupService.getGroups();
-					getGroups(grps);
+					log("creating groups...");
+					checkIfLoaded(
+						function() {
+							grps = window.groupService.getGroups();
+							grpsKeys = Object.keys(grps);
+							return grpsKeys.length < 9;
+						},
+						function() {
+							window.groupService.createGroup(reservGroupIcons[0], "My Reserve");
+							window.groupService.createGroup(reservGroupIcons[1], "Reserved");
+							checkIfLoaded(
+								function() {
+									grps = window.groupService.getGroups();
+									grpsArr = Object.keys(grps).map(k => grps[k]);
+									return grpsArr.find(f => f.icon === reservGroupIcons[0]) && grpsArr.find(f => f.icon === reservGroupIcons[1]);
+								},
+								function() {
+									grpMyReserv = db.groups.myReserv = grpsArr.find(f => f.icon === reservGroupIcons[0]);
+									grpReserv = db.groups.reserved = grpsArr.find(f => f.icon === reservGroupIcons[1]);
+									saveDB();
+								}
+							);							
+						}
+					);
 				}
-			});
+			}
+		);
 	}
 
 	var getGroups = function(groups) {
-		for (var g in groups) {
-			var grp = groups[g];
-			switch (grp.icon) {
-				case reservGroupIcons[0]:
-					grpMyReserv = grp;
-					break;
-				case reservGroupIcons[1]:
-					grpReserv = grp;
-					break;
-			}
+		if (db.groups.myReserv && db.groups.reserved && groups[db.groups.myReserv.id] && groups[db.groups.reserved.id]) {
+			grpMyReserv = db.groups.myReserv;
+			grpReserved = db.groups.reserved;
+			log("groups found:", grpMyReserv, grpReserved);
+			return true;
+		} else {
+			db.groups = {};
+			return false;
 		}
-		if (grpMyReserv && grpReserv)
-			log("groups found:", grpMyReserv, grpReserv);
 	}
 
 	var onAttachGroupToVillage = function(groupId, villageId) {
@@ -398,8 +437,8 @@ var reservations = window.reservations = (function() {
 		saveDB();
 
 		// propagate reservation for message
-		var msg = "/addreserv " + zip(villageId) + " " + zip(reserv.dt) + " ";
-		window.messagingService.reply(reserv.msg_id, msg + hash(msg + JSON.stringify(reserv)));
+		var send_msg = "/addreserv " + zip(villageId) + " " + zip(reserv.dt);
+		window.messagingService.reply(reserv.msg_id, send_msg);
 
 		$("#reservMsgs").attr('disabled', 'disabled');
 		return true;
@@ -413,8 +452,8 @@ var reservations = window.reservations = (function() {
 		// propagate delete for message
 		var reserv = db.reservations[villageId];
 		if (reserv) {
-			var msg = "/remreserv " + zip(villageId) + " ";
-			window.messagingService.reply(reserv.msg_id, msg + hash(msg + JSON.stringify(reserv)));
+			var send_msg = "/remreserv " + zip(villageId);
+			window.messagingService.reply(reserv.msg_id, send_msg);
 		}
 
 		delete db.reservations[villageId];
@@ -442,23 +481,11 @@ var reservations = window.reservations = (function() {
 				}
 			}
 			var reserv = db.reservations[villageId];
-			if (reserv || window.groupService.getVillageGroups(37042).find(f => f.id === grpMyReserv.id || f.id === grpReserv.id)) {
+			if (reserv || window.groupService.getVillageGroups(villageId).find(f => f.id === grpMyReserv.id || f.id === grpReserved.id)) {
 				$("#reservMsgs").attr('disabled', 'disabled').val((reserv) ? reserv.msg_id : 0);
 			}
 		});
-	}
-
-	var cleanDB = function() {
-		for (var r in db.reservations) {
-			var msg_id = db.reservations[r].msg_id;
-			if (!db.messages[msg_id])
-				delete db.reservations[r];
-		}
 		saveDB();
-	}
-
-	var eraseOldLocalDB = function() {
-		Object.keys(localStorage).filter(f => f.indexOf("reservDB_") > -1 && f.indexOf("reservDB_v" + dbversion + "_") < 0).forEach(e => localStorage.removeItem(e));
 	}
 
 	// Init
@@ -483,7 +510,8 @@ var reservations = window.reservations = (function() {
 
 })();
 
-reservations.Init();
-
+setTimeout(function () {
+	window.reservations.Init();
+}, 2000);
 // reservations list example:
 //[b][color=0111af][size=medium]RESERVAS:[/size] [br][br][/color][/b][b]B. Wolf[br][/b][village=17051]Império Leal - VII (488|393)[/village][br][b][br]pooh1[br][/b][village=19625]Camp 04 (484|400)[/village][br][b][br]FGOES[br][/b][village=30045]VI Marcelor (490|391)[/village][br][b][br]Rodrigo Barbaro 001[br][/b][village=17050]danispk68-3 (483|392)[/village] noblada [br][b][br]gerotto[br][/b][village=13756]danispk68-4 (483|396)[/village][br][village=13751]V Distrito de Marcelo I (482|401)[/village][br][village=13728]IV Capitania de Marcelo I (484|401)[/village][br][b][br]DullBoy[br][/b][village=29164]V  Provincia de Marcelo (484|394)[/village][br][b][br]K4m4l[/b][br][village=29587]XII GROTOVISK (479|393)[/village][br][b][br]felicosta[br][/b][village=13733]VII Marcelor (488|389)[/village][br][village=27668]IIIProvíncia de Marcelo I (486|398)[/village][br][b][br]Romops[br][/b][village=24569]Capitania de Marcelo I (466|383)[/village][br][village=33855]II Distrito Marcelo I (467|384)[/village][br][b][br]Ancient Felipe[br][/b][village=18098]Aldeia de Anderson felipelli (485|391)[/village][br][b][br]Joanna Dark[br][/b][village=15856]03. EraModern (486|396)[/village][br][village=32630]IV Distrito de Marcelo I (482|382)[/village][br][b][br]Jon Rakar[br][/b][village=30046]Aldeia Bárbara (484|391)[/village][br][b][br]geison neno[br][/b][village=30013]III Condado de Marcelo (475|393)[/village][br][br][b]vladgmania[/b][br][village=26608]II Província de Marcelo I (487|400)[/village][br][village=27649]IV Província de Marcelo I (487|398)[/village] 
